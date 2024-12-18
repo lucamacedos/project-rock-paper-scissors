243cda63304c056dc4aaf88640aaa21e9bb91225
Lucas Macedo <lucas-macedo-2009@hotmail.com>
2024-12-18 18:23:33 -0300

    Add 5 time round in script.js

--- script.js
+++ script.js
@@ -16,28 +16,36 @@ function getHumanChoice() {
   return humanChoice
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase()
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+  
+    if ((humanChoice === "paper" && computerChoice === "rock") 
+      || (humanChoice === "rock" && computerChoice === "scissors") 
+      ||  (humanChoice === "scissors" && computerChoice === "paper")){
+      humanScore += 1;
+      return alert(`You Win! ${humanChoice} beats ${computerChoice}.`);
+    } else if ((humanChoice === "rock" && computerChoice === "paper")
+      || (humanChoice === "scissors" && computerChoice === "rock")
+      || (humanChoice === "paper" && computerChoice === "scissors")) {
+        computerScore += 1;
+        return alert(`You Lose! ${humanChoice} don't beats ${computerChoice}.`);
+      } else {
+        return alert(`Draw! ${humanChoice} and ${computerChoice} it's the same.`)
+      }
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
 
-  if ((humanChoice === "paper" && computerChoice === "rock") 
-    || (humanChoice === "rock" && computerChoice === "scissors") 
-    ||  (humanChoice === "scissors" && computerChoice === "paper")){
-    humanScore += 1;
-    return alert(`You Win! ${humanChoice} beats ${computerChoice}.`);
-  } else if ((humanChoice === "rock" && computerChoice === "paper")
-    || (humanChoice === "scissors" && computerChoice === "rock")
-    || (humanChoice === "paper" && computerChoice === "scissors")) {
-      computerScore += 1;
-      return alert(`You Lose! ${humanChoice} don't beats ${computerChoice}.`);
-    } else {
-      return alert(`Draw! ${humanChoice} and ${computerChoice} it's the same.`)
-    }
+  playRound(humanSelection, computerSelection)
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame()
+playGame()
+playGame()
+playGame()
+playGame()
